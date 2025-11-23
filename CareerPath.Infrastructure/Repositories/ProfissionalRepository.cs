d85bae1659eb5c05b22d558f8b345e2ff5d53988
M4rkitos <marviniam@outlook.com>
2025-11-23 00:07:43 -0300

    adds2

--- CareerPath.Infrastructure/Repositories/ProfissionalRepository.cs
+++ CareerPath.Infrastructure/Repositories/ProfissionalRepository.cs
@@ -1,14 +1,14 @@
-// Arquivo: ProfissionalRepository.cs
-// Localização: CareerPath.Infrastructure/Repositories/ProfissionalRepository.cs
-
 using CareerPath.Application.Interfaces;
 using CareerPath.Domain.Entities;
 using CareerPath.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CareerPath.Infrastructure.Repositories
 {
-    // ProfissionalRepository implementa a interface específica
     public class ProfissionalRepository : IProfissionalRepository
     {
         private readonly ApplicationDbContext _context;
@@ -18,19 +18,48 @@ namespace CareerPath.Infrastructure.Repositories
             _context = context;
         }
 
-        // Implementação do método específico
         public async Task<Profissional?> GetByEmailAsync(string email)
         {
             return await _context.Profissionais
                                  .FirstOrDefaultAsync(p => p.Email == email);
         }
 
-        // Implementação dos métodos herdados do IRepository (exemplo: Add)
-        public async Task AddAsync(Profissional profissional)
+        public async Task AddAsync(Profissional entity)
         {
-            await _context.Profissionais.AddAsync(profissional);
+            await _context.Profissionais.AddAsync(entity);
         }
-        
-        // ... (Implementar os demais métodos do CRUD e SaveChangesAsync)
+
+        public async Task<Profissional?> GetByIdAsync(Guid id)
+        {
+            return await _context.Profissionais.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Profissional>> GetAllAsync()
+        {
+            return await _context.Profissionais.ToListAsync();
+        }
+
+        public void Update(Profissional entity)
+        {
+            _context.Profissionais.Update(entity);
+        }
+
+        public void Delete(Profissional entity)
+        {
+            _context.Profissionais.Remove(entity);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        // Método exigido pela interface que estava faltando
+        public async Task<IEnumerable<Profissional>> GetByProfissaoAndExperiencia(string profissao, int minExperiencia)
+        {
+            return await _context.Profissionais
+                .Where(p => p.ProfissaoAtual == profissao && p.AnosExperiencia >= minExperiencia)
+                .ToListAsync();
+        }
     }
 }
